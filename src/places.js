ece5972de036a9ae8007022f8147ce72cbcc5937
berger <berger@GEOD.BAU.TU-BS.DE>
2021-10-14 11:59:38 +0200

    AR.js commit

---
+++ src/places.js
@@ -0,0 +1,72 @@
+window.onload = () => {
+
+    let places = staticLoadPlaces();
+    renderPlaces(places);
+};
+
+
+function staticLoadPlaces() {
+    return [
+        {
+            name: "IGP",
+            location: {
+                lat: 52.286220, // add here latitude if using static data
+                lng: 10.539285, // add here longitude if using static data
+            }
+        },
+        {
+            name: 'Box',
+            location: {
+                lat: 52.286082,
+                lng: 10.539348,
+            }
+        }
+    ];
+}
+
+
+function renderPlaces(places) {
+    let scene = document.querySelector('a-scene');
+
+    places.forEach((place) => {
+        const latitude = place.location.lat;
+        const longitude = place.location.lng;
+
+        // add place icon
+        const icon = document.createElement('a-image');
+        icon.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude}`);
+        icon.setAttribute('name', place.name);
+        icon.setAttribute('src', '../images/map-marker.png');
+
+        // for debug purposes, just show in a bigger scale, otherwise I have to personally go on places...
+        icon.setAttribute('scale', '20, 20');
+
+        icon.addEventListener('loaded', () => window.dispatchEvent(new CustomEvent('gps-entity-place-loaded')));
+
+        const clickListener = function (ev) {
+            ev.stopPropagation();
+            ev.preventDefault();
+
+            const name = ev.target.getAttribute('name');
+
+            const el = ev.detail.intersection && ev.detail.intersection.object.el;
+
+            if (el && el === ev.target) {
+                const label = document.createElement('span');
+                const container = document.createElement('div');
+                container.setAttribute('id', 'place-label');
+                label.innerText = name;
+                container.appendChild(label);
+                document.body.appendChild(container);
+
+                setTimeout(() => {
+                    container.parentElement.removeChild(container);
+                }, 1500);
+            }
+        };
+
+        icon.addEventListener('click', clickListener);
+
+        scene.appendChild(icon);
+    });
+}
